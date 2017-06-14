f7f10406d6054902b9eb50df415e70aa3391cd27
wuhouz <dahan8907@163.com>
2017-06-14 18:24:16 +0800

    robot

---
+++ clip_img.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $("#img1,#img2,#img3").hover(function () {
+        $(this).addClass("clip");
+    }, function () {
+        $(this).removeClass("clip");
+    });
+
+    $("#img_before1").hide(); $("#img_before2").hide(); $("#img_before3").hide();
+
+    $("#img_container1").hover(function () {
+        $("#img_before1").show();
+    }, function () {
+        $("#img_before1").hide();
+    });
+    $("#img_container2").hover(function () {
+        $("#img_before2").show();
+    }, function () {
+        $("#img_before2").hide();
+    });
+    $("#img_container3").hover(function () {
+        $("#img_before3").show();
+    }, function () {
+        $("#img_before3").hide();
+    });
+});
